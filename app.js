a9c14cbedd57561d3825d94bf60d0f7d7e97bb88
Raph <raph4sure007@gmail.com>
2025-10-14 14:54:28 +0100

    trying to edit the database connection for render to be able to access it

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(function (err, req, res, next) {
 db.getConnection()
     .then(() => {
         console.log("Connected to the database");
-        const port = process.env.DB_PORT || 3001;
+        const port = process.env.PORT || 3001;
         app.listen(port, () => {
             console.log(`Server running on port ${port}`);
         });
