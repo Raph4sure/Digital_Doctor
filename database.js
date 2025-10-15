fef35deab8de49e59ca8e89818a0e711ea4ea8d3
Raph <raph4sure007@gmail.com>
2025-10-15 02:14:54 +0100

    included db_port in database.js

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -16,7 +17,7 @@ const pool = mysql.createPool({
 
 
 
-connection.getConnection((err) => {
+pool.getConnection((err) => {
     if (err) console.error("Error connecting to the database", err);
     else console.log("Connected to the database");
 });
