3bd996ad55d3ff5af09b637f6a72527fc3fd6740
Raph <raph4sure007@gmail.com>
2025-10-15 02:21:58 +0100

    rejecting ssl in db.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, 
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
     ssl: {
-        ca: fs.readFileSync(__dirname + "/ca.pem"),
+        rejectUnauthorized: false,
     },
 });
 
